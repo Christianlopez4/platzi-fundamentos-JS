f37b566c8cade47b842205a54de58e434a4dda3c
christianlopez4 <christian.lopez4@correo.icesi.edu.co>
2021-01-14 19:45:08 -0500

    Ilumino la secuencia de colores

--- Simon dice.js
+++ Simon dice.js
@@ -4,11 +4,11 @@ const violeta = document.getElementById('violeta');
 const naranja = document.getElementById('naranja');
 const verde = document.getElementById('verde');
 
-
   class Juego {
     constructor(){
       this.inicializar();
       this.generarSecuencia();
+      this.siguienteNivel();
     }
 
     inicializar() {
@@ -21,6 +21,39 @@ const verde = document.getElementById('verde');
 
     generarSecuencia() {
         this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+    }
+
+    siguienteNivel() {
+        this.iluminarSecuencia();
+    }
+
+    transformarNumeroAColor(num) {
+        switch (num) {
+            case 0:
+                return 'celeste';
+            case 1: 
+                return 'naranja';
+            case 2: 
+                return 'verde';
+            case 3:
+                return 'violeta';
+        }
+    }
+
+    iluminarSecuencia() {
+        for (let i = 0; i < this.nivel; i++) {
+            let color = this.transformarNumeroAColor(this.secuencia[i]);
+            setTimeout(() => this.iluminarColor(color), 1000 * i);
+        }
+    }
+
+    iluminarColor(color) {
+        this.colores[color].classList.add('light');
+        setTimeout(() => this.apagarColor(color), 350);
+    }
+
+    apagarColor(color) {
+        this.colores[color].classList.remove('light');
     }
 
   }
